2e48199ec9626e9f74d6eadab2d0cd37c3197099
rmckenzie <rmckenzie92@gmail.com>
2012-04-11 00:45:32 -0500

    A lot of naming and formatting changes, mostly readability cleanups

--- compiler.py
+++ compiler.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 import sys
-from pprint import pprint
-from parse import parse
-from IRtoAsm import toasm, tobytes
-from optimize import optimizeStmts
-from astrepr import *
+
+from .parse import parse
+from .IRtoAsm import toasm, tobytes
+from .optimize import optimizeStmts
+from .astrepr import *
 
 DEBUG = False
 OPTIMIZE = True
-
 var_name = 0
+
+
 def gensym():
     """
     Gives a unique name to a variable for the SSA IR.
@@ -17,7 +18,8 @@ def gensym():
 
     global var_name
     var_name += 1
-    return '_r'+str(var_name-1)
+    return '_r' + str(var_name - 1)
+
 
 def flattenFunc(prog):
     """
@@ -30,77 +32,103 @@ def flattenFunc(prog):
     res = []
     for instr in prog:
         if instr[0] == IF:
-            a,b,c = gensym(), gensym(), gensym()
+            a, b, c = gensym(), gensym(), gensym()
             ift = flattenFunc(instr[2])
             iff = flattenFunc(instr[3])
-            stmt = [(IR+IF, instr[1]), (GOTO, a)]
+            stmt = [(IR + IF, instr[1]), (GOTO, a)]
             stmt += iff + [(GOTO, c)]
             stmt += [(LABEL, a)] + ift
             stmt += [(LABEL, c)]
             res += stmt
+
         elif instr[0] == WHILE:
             test = gensym()
             top = gensym()
-            stmt = [(GOTO, test), (LABEL, top)]+flattenFunc(instr[2])
-            stmt += [(LABEL, test), (IR+IF, instr[1]), (GOTO, top)]
+            stmt = [(GOTO, test), (LABEL, top)] + flattenFunc(instr[2])
+            stmt += [(LABEL, test), (IR + IF, instr[1]), (GOTO, top)]
             res += stmt
+
         elif instr[0] == FOR:
             test = gensym()
             top = gensym()
             stmt = [instr[1]]
-            stmt += [(GOTO, test), (LABEL, top)]+flattenFunc(instr[4])
+            stmt += [(GOTO, test), (LABEL, top)] + flattenFunc(instr[4])
             stmt += [instr[3]]
-            stmt += [(LABEL, test), (IR+IF, instr[2]), (GOTO, top)]
+            stmt += [(LABEL, test), (IR + IF, instr[2]), (GOTO, top)]
             res += stmt
+
         else:
             # Don't do anything special
             res.append(instr)
 
     return res
 
+
 def flattenStmt(exp, putres=None):
     """
     Generate the static single assignment (SSA) form.
-    This isn't strictly SSA because we don't rebind named variables to unique names.
-    This makes computing the address of the C variables much easier to compute.
-    
+    This isn't strictly SSA because we don't rebind named variables to unique
+    names. This makes computing the address of the C variables much easier to
+    compute.
     """
     if exp[0] == ASSIGN:
         if exp[1][0] == VAR:
             return flattenStmt(exp[2], exp[1][1])
+
         elif exp[1][0] == DEREF:
-            a,b = gensym(),gensym()
-            return flattenStmt(exp[1][1], a)+flattenStmt(exp[2], b)+[(IR+ASSIGN, b, a)]
-    if exp[0] in [LIT, VAR, STRING]:
-        return [(IR+exp[0], exp[1], putres)]
-    if exp[0] == ADDR:
+            a, b = gensym(), gensym()
+            return (flattenStmt(exp[1][1], a) +
+                    flattenStmt(exp[2], b) +
+                    [(IR + ASSIGN, b, a)])
+
+    elif exp[0] in [LIT, VAR, STRING]:
+        return [(IR + exp[0], exp[1], putres)]
+
+    elif exp[0] == ADDR:
         a = gensym()
-        return flattenStmt(exp[1], a)+[(IR+ADDR, a, putres)]
-    if exp[0] == DEREF:
+        return flattenStmt(exp[1], a) + [(IR + ADDR, a, putres)]
+
+    elif exp[0] == DEREF:
         a = gensym()
-        return flattenStmt(exp[1], a)+[(IR+DEREF, a, putres)]
-    if exp[0] in [PREINC, POSTINC]:
+        return flattenStmt(exp[1], a) + [(IR + DEREF, a, putres)]
+
+    elif exp[0] in [PREINC, POSTINC]:
         assert exp[1][0] == 'VAR'
-        a = (IR+PLUS, exp[1][1], 1, exp[1][1])
-        b = (IR+MOV, exp[1][1], putres)
-        if putres == None: return [a]
-        return [a,b] if exp[0] == PREINC else [b,a]
-    if exp[0] == RETURN:
+        a = (IR + PLUS, exp[1][1], 1, exp[1][1])
+        b = (IR + MOV, exp[1][1], putres)
+
+        if not putres:
+            return [a]
+
+        elif exp[0] == PREINC:
+            return [a, b]
+
+        else:
+            return [b, a]
+
+    elif exp[0] == RETURN:
         if exp[1] == None:
-            return [(IR+RETURN, None)]
-        a = gensym()
-        return flattenStmt(exp[1], a)+[(IR+RETURN, a)]
-    if exp[0] in [PLUS, MINUS, MULTIPLY, DIVIDE, REMAINDER, EQ, NEQ, GR, GE,
+            return [(IR + RETURN, None)]
+        else:
+            a = gensym()
+            return flattenStmt(exp[1], a) + [(IR + RETURN, a)]
+
+    elif exp[0] in [PLUS, MINUS, MULTIPLY, DIVIDE, REMAINDER, EQ, NEQ, GR, GE,
                   SHIFTL, SHIFTR, BOOLAND, BOOLOR, BOOLXOR]:
-        a,b = gensym(), gensym()
-        return flattenStmt(exp[1],a)+flattenStmt(exp[2],b)+[(IR+exp[0], a, b, putres)]
-    if exp[0] == IR+IF:
+        a, b = gensym(), gensym()
+        return (flattenStmt(exp[1], a) +
+                flattenStmt(exp[2], b) +
+                [(IR + exp[0], a, b, putres)])
+
+    elif exp[0] == IR + IF:
         a = gensym()
-        return flattenStmt(exp[1], a)+[(IR+IF, a)]
-    if exp[0] in [PRINT, PUTCHAR]:
+        return flattenStmt(exp[1], a) + [(IR + IF, a)]
+
+    elif exp[0] in [PRINT, PUTCHAR]:
         a = gensym()
-        return flattenStmt(exp[1],a)+[(IR+exp[0], a)]
-    if exp[0] == CALL:
+        return flattenStmt(exp[1], a) + [(IR + exp[0], a)]
+
+    elif exp[0] == CALL:
         r = []
         syms = []
         for arg in exp[2]:
@@ -108,9 +136,9 @@ def flattenStmt(exp, putres=None):
             r += flattenStmt(arg, n)
             syms.append(n)
         v = gensym()
-        return flattenStmt(exp[1], v)+r+[(IR+CALL, v, syms, putres)]
-    return [exp]
+        return flattenStmt(exp[1], v) + r + [(IR + CALL, v, syms, putres)]
 
+    return [exp]
 
 
 def comp(prog):
@@ -123,7 +151,7 @@ def comp(prog):
     varList = {}
     # The list of strings we need to allocate, or change the pointers of
     heapalloc = []
-    OFFSET = 4 # words needed to have the startup code
+    OFFSET = 4  # words needed to have the startup code
 
     # Instructions we need to add to main to initialize globals.
     addToMain = []
@@ -135,83 +163,90 @@ def comp(prog):
             addToMain.append(func)
             continue
         else:
-            funcList[func[1][1]] = len(res)+OFFSET
+            funcList[func[1][1]] = len(res) + OFFSET
 
         code = func[3]
 
         if func[1][1] == 'main':
             # TODO this is ugly.
-            startup = tobytes(("JSR", 0x1F, len(res)+OFFSET))+[0x7dc1, 2] # first jump to main, then goto 2
+            # first jump to main, then goto 2
+            startup = tobytes(("JSR", 0x1F, len(res) + OFFSET)) + [0x7dc1, 2]
             code = addToMain + code
-            
-        func1 = flattenFunc(code)+[(RETURN, None)]
-        func2 = sum(map(flattenStmt,func1), [])
+
+        func1 = flattenFunc(code) + [(RETURN, None)]
+        func2 = sum(map(flattenStmt, func1), [])
+
         if OPTIMIZE:
             func2 = optimizeStmts(func2, DEBUG)
-        func3,ha = toasm(func2, [x[1] for x in func[2]], funcList, varList, len(res)+OFFSET, DEBUG)
-        heapalloc += [(a+len(res)+OFFSET,b,c) for a,b,c in ha]
+
+        func3, ha = toasm(func2, [x[1] for x in func[2]],
+                          funcList, varList, len(res) + OFFSET, DEBUG)
+
+        heapalloc += [(a + len(res) + OFFSET, b, c) for a, b, c in ha]
         res += func3
 
-    res = startup+res
-    end = len(res)
-
+    res = startup + res
 
     # Here is where we fix any pointers to heap data.
     heap = []
-    for change,kind,obj in heapalloc:
+    for change, kind, obj in heapalloc:
         if kind == "STRING":
-            nxt = [ord(x) for x in obj]+[0]
-            res[change] = len(res)+len(heap)
+            nxt = [ord(x) for x in obj] + [0]
+            res[change] = len(res) + len(heap)
             heap += nxt
         if kind == "INTEGER":
             if change == None:
                 # This is the init telling us to make the space.
-                varList[obj] = len(res)+len(heap)
+                varList[obj] = len(res) + len(heap)
                 heap += [0]
             else:
                 res[change] = varList[obj]
-            
 
     return res, heap
 
 
-def run(prog):
-    if DEBUG: print "PROG IS", prog
-    prog = parse(prog, DEBUG)
-    prog,heap = comp(prog)
-    if DEBUG: print "NUMBYTES:", len(prog)
-    if DEBUG: print
-    return prog+heap
-    
+def run(prog, debug=False):
+    prog = parse(prog, debug)
+    prog, heap = comp(prog)
+
+    if debug:
+        print "PROG IS", prog
+        print "NUMBYTES:", len(prog)
+
+    return (prog + heap)
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print 'Usage: python compiler.py [-n] [-d] [-o outfile] input'
-        print '   -n disables optimizations'
-        print '   -d print debugging information about the compilation'
-        print '   -o outfile writes the output to the given file'
+        print 'Usage: python compiler.py [-n] [-d] [-o outfile] input' +\
+              '   -n disables optimizations' +\
+              '   -d print debugging information about the compilation' +\
+              '   -o outfile writes the output to the given file' +\
         exit(0)
-    i = 1
-    fname = None
-    while i != len(sys.argv)-1:
-        if sys.argv[i] == '-n':
-            OPTIMIZE = False
-            i += 1
-        if sys.argv[i] == '-o':
-            fname = sys.argv[i+1]
-            i += 2
-        if sys.argv[i] == '-d':
-            DEBUG = True
-            i += 1
-    
-    if fname == None:
-        fname = 'a.out'
 
-    prog = open(sys.argv[i]).read()
-    res = run(prog)
-    
-    if DEBUG: print " ".join(a)
-    
-    a = ["%04x"%x for x in res]
-    open(fname, "w").write((" ".join(a)))
+    elif len(sys.argv > 1):
+        i = 1
+        fname = None
+        while i != (len(sys.argv) - 1):
+            if sys.argv[i] == '-n':
+                OPTIMIZE = False
+                i += 1
+            if sys.argv[i] == '-o':
+                fname = sys.argv[i + 1]
+                i += 2
+            if sys.argv[i] == '-d':
+                DEBUG = True
+                i += 1
+
+        if fname == None:
+            fname = 'a.out'
+
+        prog = open(sys.argv[i]).read()
+        res = run(prog)
+
+        if DEBUG:
+            print " ".join(a)
+
+        a = ["%04x" % x for x in res]
+        open(fname, "w").write((" ".join(a)))
 
--- optimize.py
+++ optimize.py
@@ -3,6 +3,7 @@ from IRtoAsm import getVarLocations
 
 DEBUG = False
 
+
 def optimize_unusedVars(stmts):
     """
     When we go var1 -> var2, just use var1 instead of var2.
@@ -13,10 +14,12 @@ def optimize_unusedVars(stmts):
     rest = []
     equal = {}
     for stmt in stmts:
-        if stmt[0] == IR+VAR and stmt[2][0] == '_':
+        if(stmt[0] == IR + VAR) and (stmt[2][0] == '_'):
             equal[stmt[2]] = stmt[1]
-        elif stmt[0] == IR+LIT and stmt[2][0] == '_':
+
+        elif(stmt[0] == IR + LIT) and (stmt[2][0] == '_'):
             equal[stmt[2]] = stmt[1]
+
         else:
             rest.append(stmt)
 
@@ -24,14 +27,16 @@ def optimize_unusedVars(stmts):
         return equal[x] if x in equal else x
 
     for i in range(len(rest)):
-        if rest[i][0] == IR+CALL:
-            rest[i] = (rest[i][0], fix(rest[i][1]), map(fix, rest[i][2]), fix(rest[i][3]))
+        if(rest[i][0] == IR + CALL):
+            rest[i] = (rest[i][0], fix(rest[i][1]),
+                        map(fix, rest[i][2]), fix(rest[i][3]))
         else:
             rest[i] = tuple([rest[i][0]] + map(fix, rest[i][1:]))
 
     if DEBUG: print "AFTER", len(rest)
     if DEBUG: pprint(rest)
     return rest
+
 
 def optimize_if(stmts):
     """
@@ -40,54 +45,64 @@ def optimize_if(stmts):
     """
     res = []
     i = 0
-    while i < len(stmts)-2:
-        if stmts[i+1][0] == IR+IF and stmts[i+2][0] == GOTO:
-            if stmts[i][0] in [IR+EQ, IR+NEQ, IR+GR, IR+GE]:
+    while i < (len(stmts) - 2):
+        if (stmts[i + 1][0] == IR + IF) and (stmts[i + 2][0] == GOTO):
+            if stmts[i][0] in [IR + EQ, IR + NEQ, IR + GR, IR + GE]:
                 res.append((stmts[i][0], stmts[i][1], stmts[i][2], None))
                 i += 1
+
             else:
                 res.append(stmts[i])
         else:
             res.append(stmts[i])
         i += 1
-    return res+stmts[-2:]
+    return res + stmts[-2::]
 
 
 def replaceVarNames(instr, newnames):
     """
     Use the newnames function to find the new name for the SSA variables.
     """
-    res = []
-    if instr[0] in [IR+IF, IR+PRINT, IR+PUTCHAR, IR+RETURN]:
+    if instr[0] in [IR + IF, IR + PRINT, IR + PUTCHAR, IR + RETURN]:
         return (instr[0], newnames(instr[1]))
-    if instr[0] in [IR+VAR, IR+LIT, IR+ASSIGN, IR+DEREF, IR+ADDR]:
+
+    elif instr[0] in [IR + VAR, IR + LIT, IR + ASSIGN, IR + DEREF, IR + ADDR]:
         return (instr[0], newnames(instr[1]), newnames(instr[2]))
-    if instr[0] == IR+STRING:
+
+    elif instr[0] == IR + STRING:
         return (instr[0], instr[1], newnames(instr[2]))
-    if instr[0] in [IR+PLUS, IR+MINUS, IR+MULTIPLY, IR+DIVIDE, IR+REMAINDER, 
-                    IR+EQ, IR+NEQ, IR+GR, IR+GE, IR+SHIFTL, IR+SHIFTR, IR+BOOLAND, 
-                    IR+BOOLOR, IR+BOOLXOR]:
-        return (instr[0], newnames(instr[1]), newnames(instr[2]), newnames(instr[3]))
-    if instr[0] == IR+CALL:
-        return (instr[0], newnames(instr[1]), map(newnames,instr[2]), newnames(instr[3]))
+
+    elif instr[0] in [IR + PLUS, IR + MINUS, IR + MULTIPLY, IR + DIVIDE,
+                      IR + REMAINDER, IR + EQ, IR + NEQ, IR + GR, IR + GE,
+                      IR + SHIFTL, IR + SHIFTR, IR + BOOLAND, IR + BOOLOR,
+                      IR + BOOLXOR]:
+        return (instr[0], newnames(instr[1]), newnames(instr[2]),
+                newnames(instr[3]))
+
+    if instr[0] == IR + CALL:
+        return (instr[0], newnames(instr[1]), map(newnames, instr[2]),
+                newnames(instr[3]))
+
     return instr
+
 
 def reuse_vars(stmts):
     """
     Convert away from SSA to use the same memory locations when we can.
-    Doesn't do it in any intelligent manner right now -- just pick the first open spot.
-    This helps us allocate registers as well.
+    Doesn't do it in any intelligent manner right now -- just pick the first
+    open spot. This helps us allocate registers as well.
     """
     locations = []
     newnames = {}
 
     def firstFree():
         for i in range(len(locations)):
-            if locations[i] == None: return i
+            if locations[i] == None:
+                return i
         locations.append(None)
-        return len(locations)-1
+        return len(locations) - 1
 
-    for i,stmt in enumerate(stmts):
+    for i, stmt in enumerate(stmts):
         tofree = []
         for v in getVarLocations(stmt):
             if type(v) == type("") and v[0] == '_':
@@ -96,17 +111,17 @@ def reuse_vars(stmts):
                 else:
                     ff = firstFree()
                     locations[ff] = v
-                    newnames[v] = "_t"+str(ff)
+                    newnames[v] = "_t" + str(ff)
         for each in tofree:
             locations[each] = None
 
-                    
     def rnewnames(x):
         if x in newnames:
             return newnames[x]
         else:
             return x
     return [replaceVarNames(x, rnewnames) for x in stmts]
+
 
 def optimizeStmts(stmts, debug=False):
     global DEBUG
